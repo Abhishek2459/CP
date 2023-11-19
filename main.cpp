d6b60ac3ef167c567d70388e4d135f7ab294af5a
abhishekpatra <abhishekpatra6129@gmail.com>
2023-11-19 17:20:57 +0530

    c2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+using namespace std;
 
-int main()//(int argc, const char * argv[]) 
-{
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+  cout << "Hello World!";
+  return 0;
 }
